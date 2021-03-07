221ac06c06d2af6b7824ed858cd6fbfce6517774
xiayang <xiayang_yanshan@163.com>
2021-03-08 07:37:52 +0800

    learning

---
+++ freemind/政治经济学.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1604746550733" ID="ID_1024199578" MODIFIED="1604746592732">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#133aeb" face="Times New Roman">&#25919;&#27835;&#32463;&#27982;&#23398;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="18"/>
+<node CREATED="1604746724924" ID="ID_765475162" MODIFIED="1604746724924" POSITION="right" TEXT="">
+<node CREATED="1604746728947" ID="ID_1642293646" MODIFIED="1604746736832">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/11.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1604747225078" ID="ID_1466679589" MODIFIED="1604747233029">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="pictures/12.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
